9fdc2c58436806c8458691da3503cc09489053c1
Matthew Cross <matthew@pmg.co>
2015-05-10 17:29:11 -0500

    Fix printf statements

--- src/main/scala/simplerpg/player/Item.scala
+++ src/main/scala/simplerpg/player/Item.scala
@@ -11,19 +11,19 @@ import com.owlike.genson.defaultGenson._
 sealed abstract class SellableObject
 
 case class Item(name: String, price: Double, effects: Vitals) extends SellableObject {
-    override def toString(): String = s"- $name   ($price)   ${effects.toString}"
+    override def toString(): String = f"$name%32s ($price%08.2f) ${effects.toString}"
 }
 
 case class Weapon(name: String, price: Double, damage: Int, var isEquipped: Boolean) extends SellableObject {
     override def toString(): String = {
-        val message = s"- $name   ($price)   ${damage}pt   "
+        val message = f"$name%32s ($price%08.2f) ${damage}pt   "
         message + (if (isEquipped) "Equipped" else "")
     }
 }
 
 case class Armor(name: String, price: Double, physicalProtection: Int, magicProtection: Int, var isEquipped: Boolean) extends SellableObject {
     override def toString(): String = {
-        val message = s"- $name   ($price)   ${physicalProtection}pt   ${magicProtection}pt   "
+        val message = f"$name%32s ($price%08.2f) Phys: ${physicalProtection}pt Mag: ${magicProtection}pt "
         message + (if (isEquipped) "Equipped" else "")
     }
 }
